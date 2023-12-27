4877802e63f504e1e9f64350b56bd34950564cf9
Justin Horein <justin@Justins-MacBook-Pro.local>
2023-12-26 21:23:49 -0600

    app display planet data!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [planet, setPlanet] = useState('planet');
+  const [body, setBody] = useState();
+
+  const fetchData = (planet) => {
+    var display;
+    let data = localStorage.getItem("planets");
+    data = JSON.parse(data);
+  
+    if (!data) {
+      axios({
+        method: "get",
+        url: "https://api.le-systeme-solaire.net/rest/bodies",
+      })
+      .then(function(res) {
+        let str = JSON.stringify(res.data);
+        localStorage.setItem("planets", str);
+        data = localStorage.getItem("planets");
+        data = JSON.parse(data);
+      })
+    }
+    
+    data.bodies.forEach(body => {
+      // console.log(body);
+      if (body.englishName == planet) {
+        display = JSON.stringify(body);
+      }
+    });
+    
+    setBody(display);
+  }
+
+  const handleSelect = (event) => {
+    let planet = event.target.value;
+    fetchData(planet);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={handleSelect}>
+        <option value="Earth">Earth</option>
+        <option value="Moon">Moon</option>
+        <option value="Sun">Sun</option>
+      </select>
+
+      <div>{ body }</div>
     </div>
   );
 }
